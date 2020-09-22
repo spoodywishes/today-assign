020ebd0467d30028317a39923dfe1f68bcedc284
spoodywishes <71467971+spoodywishes@users.noreply.github.com>
2020-09-22 17:06:31 +0100

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,18 @@
+package day_2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		System.out.println("1-My name is Ali Yalcin");
+		System.out.println("2-I am beginner SDET!!");
+		System.out.println("3-I really hope");
+		System.out.println("4-I am doing ok with this course");
+		System.out.println("5-Please dont laugh :)");
+		System.out.println("6-Because it is not funny");
+		System.out.println("7- To laugh at struggling man!!");
+		System.out.println("8- However we all been there");
+		System.out.println("9-So come on lets laugh together");
+		System.out.println("\"10-Because people smiles lives longer :))\"");
+	}
+
+}
---
+++ Question6.java
@@ -0,0 +1,22 @@
+package day_2;
+
+public class Question6 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("    ^");
+		System.out.println("  /   \\ ");
+		System.out.println(" /     \\");
+		System.out.println("/       \\");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("|       |");
+		System.out.println("_________");
+	}
+
+}
